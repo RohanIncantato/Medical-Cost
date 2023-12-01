01ec6050309e2dc7394c60fa3d4090ddc3b51136
RohanIncantato <106857699+RohanIncantato@users.noreply.github.com>
2023-12-01 19:11:45 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open('C:/Users/ROHAN/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('webapp.py', 'rb'))
 
 def medical_prediction(input_data):
     
