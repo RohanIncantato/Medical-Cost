1d1b442575e984cca5e2449d0a862ce32a19491e
RohanIncantato <106857699+RohanIncantato@users.noreply.github.com>
2023-12-01 18:24:46 +0530

    Update medicalcostwebapp.py

--- medicalcostwebapp.py
+++ medicalcostwebapp.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('medicalcostwebapp.py', 'rb'))
 
 def medical_prediction(input_data):
     
