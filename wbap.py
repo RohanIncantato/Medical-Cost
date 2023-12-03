01450abc62f9bd7544f93e4731ee48f692540080
RohanIncantato <106857699+RohanIncantato@users.noreply.github.com>
2023-12-03 22:50:16 +0530

    Update wbap.py

--- wbap.py
+++ wbap.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 #loading the saved model
-loaded_model=pickle.load(open('D:/ML project/newtry/trained_model.sav','rb'))
+loaded_model=pickle.load(open('trained_model.sav','rb'))
 
 
 def medical_prediction(input_data):
